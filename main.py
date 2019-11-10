2e8837965bae552b01d0fc1e9a7f6e02c8703805
ManuelBejarano <43890553+ManuelBejarano@users.noreply.github.com>
2019-11-09 22:22:16 -0500

    Update main.py
    Add an operation and improve the code style

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov  9 17:36:29 2019
-Assignment2
+Assignment 2 - Ingenieria de software II
 @author: Daniel Perez - Manuel Bejarano.
 """
 class Matematicas():
 
-    def operation(self,x,y) -> str:
+    def operation(self, x, y) -> str:
         pass
 
 
 class Default(Matematicas):
 
-    def operation(self,x,y) -> int:
-        aux=x+y
-        return f"Suma= {aux}"
+    def operation(self, x, y) -> int:
+        aux = x + y
+        return f"Suma = {aux}"
 
 
 class Decorator(Matematicas):
@@ -29,53 +29,66 @@ class Decorator(Matematicas):
         return self._componente
 
     def operation(self, x, y) -> str:
-        self._componente.operation(x,y)
+        self._componente.operation(x, y)
 
 
 class Multiplicacion(Decorator):
 
-    def operation(self, x,y) -> str:
-        aux=x*y        
-        return f"Multiplicacion= {aux}, {self.componente.operation(x,y)}"
+    def operation(self, x, y) -> str:
+        aux = x * y        
+        return f"Multiplicacion = {aux}, {self.componente.operation(x, y)}"
 
 
 class Resta(Decorator):
     
-    def operation(self,x,y) -> str:
-        aux=x-y
-        return f"Resta= {aux}, {self.componente.operation(x,y)}"
+    def operation(self, x, y) -> str:
+        aux = x - y
+        return f"Resta = {aux}, {self.componente.operation(x, y)}"
+
+class Division(Decorator):
+
+    def operation(self, x, y) -> str:
+
+        if y != 0:
+            aux = x / y
+            return f"Division = {aux}, {self.componente.operation(x, y)}"
+        else:    
+            return f"Division = No es posible dividir entre 0, {self.componente.operation(x, y)}"
 
 def Verificar() -> int:
-    aux=True
+
+    aux = True
+
     while(aux):
         try:
-            x=int(input())
-            aux=False
+            x = int(input())
+            aux = False
         except:
             print("Digite un numero valido:")
     return x
         
 
 def client_code(componente: Matematicas, x, y) -> None:    
-    print(f"{componente.operation(x,y)}", end="")
+    print(f"{componente.operation(x, y)}", end = "")
 
     
-
-
 if __name__ == "__main__":
     
     print("Digite un numero:")
     x=Verificar()
+
     print("Digite otro numero:")
     y=Verificar()
     
     simple = Default()
     print("Simple operacion")
+    
     client_code(simple, x, y)
     print("\n")
 
     decorator1 = Multiplicacion(simple)
     decorator2 = Resta(decorator1)
+    decorator3 = Division(decorator2)
+    
     print("Varias operaciones")
-    client_code(decorator2, x, y)
-
+    client_code(decorator3, x, y)
